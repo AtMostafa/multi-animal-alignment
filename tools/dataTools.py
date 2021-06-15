c1595b4aae7d99ede95bb25f61834360f26bc39c
mostafa.safaie <mostafa.safaie@gmail.com>
2021-06-15 10:05:09 +0100

    add CCA_pyal

--- tools/dataTools.py
+++ tools/dataTools.py
@@ -1,6 +1,7 @@
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from scipy.linalg import qr, svd, inv
 
 import logging
@@ -79,3 +80,27 @@ def canoncorr(X:np.array, Y: np.array, fullReturn: bool = False) -> np.array:
     V = Y @ B
 
     return A, B, r, U, V
+
+def CCA_pyal(df1:pd.DataFrame, field1: str, df2:pd.DataFrame =None, field2:str =None) -> np.array:
+    """
+    Rerurns CCs between 2 fields from 2 pyalData dataframes.
+    If, `df2` is not specified, then `field2` must be valid and canoncorr will be applied on
+    2 fields in `df1`
+    
+    Returns CC, i.e., ccacnonical correlations
+    """
+    if df2 is None:
+        assert isinstance(field2,str), 'Enter a valid string in field2'
+        df2 = df1
+
+    d0 = np.concatenate(df1[field1].values, axis=0)
+    d1 = np.concatenate(df2[field2].values, axis=0)
+
+    # same number of timepoints in both matrices
+    n_samples = min ([d0.shape[0], d1.shape[0]])
+    d0 = d0[:n_samples,:]
+    d1 = d1[:n_samples,:]
+
+    CC = dt.canoncorr(d0, d1)
+
+    return CC
