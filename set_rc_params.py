55473ad131461fb5b2c5e6b9dbe5dcfa1aa47069
mostafa.safaie <mostafa.safaie@gmail.com>
2021-08-16 11:00:21 +0100

    0

--- set_rc_params.py
+++ set_rc_params.py
@@ -4,11 +4,11 @@ def set_rc_params(dictArg:dict ={}):
     matplotlib.rcParams['xtick.major.pad'] = 1
     matplotlib.rcParams['ytick.major.pad'] = 1
     matplotlib.rcParams['axes.labelpad']   = 2
-    matplotlib.rcParams['axes.titlepad']   = 3
-    matplotlib.rcParams['axes.titlesize']   = 'medium'
-    matplotlib.rcParams['axes.labelsize']   = 'small'
-    matplotlib.rcParams['xtick.labelsize']   = 'x-small'
-    matplotlib.rcParams['ytick.labelsize']   = 'x-small'
+    matplotlib.rcParams['axes.titlepad']   = 5
+    matplotlib.rcParams['axes.titlesize']   = 'x-large'
+    matplotlib.rcParams['axes.labelsize']   = 'large'
+    matplotlib.rcParams['xtick.labelsize']   = 'medium'
+    matplotlib.rcParams['ytick.labelsize']   = 'medium'
     matplotlib.rcParams['text.usetex'] = True
     
     
