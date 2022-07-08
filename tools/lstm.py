08bc32b51e434bcc8d9efa8ae963757576e80d51
AtMostafa <mostafa.safaie@gmail.com>
2022-07-08 14:29:33 +0100

    clean up LSTM code

--- tools/lstm.py
+++ tools/lstm.py
@@ -49,41 +49,20 @@ class LstmDecoder(nn.Module):
         outputs = torch.cat(outputs, dim=1)
         return outputs
 
-
-def training_loop(n_epochs, model, optimiser, loss_fn, 
+def training_loop(n_epochs, model, optimiser, loss_fn,
                   train_input, train_target, test_input, test_target):
     "training"
+    def closure():
+        "Helper function"
+        optimiser.zero_grad()
+        out = model(train_input)
+        loss = loss_fn(out, train_target)
+        loss.backward()
+        return loss
+
     for i in range(n_epochs):
-        def closure():
-            optimiser.zero_grad()
-            out = model(train_input)
-            loss = loss_fn(out, train_target)
-            loss.backward()
-            return loss
         optimiser.step(closure)
-        with torch.no_grad():
-            future = 1000
-            pred = model(test_input, future=future)
-            # use all pred samples, but only go to 999
-            loss = loss_fn(pred[:, :-future], test_target)
-            y = pred.detach().numpy()
-        # draw figures
-        plt.figure(figsize=(12,6))
-        plt.title(f"Step {i+1}")
-        plt.xlabel("x")
-        plt.ylabel("y")
-        plt.xticks(fontsize=20)
-        plt.yticks(fontsize=20)
-        n = train_input.shape[1] # 999
-        def draw(yi, colour):
-            plt.plot(np.arange(n), yi[:n], colour, linewidth=2.0)
-            plt.plot(np.arange(n, n+future), yi[n:], colour+":", linewidth=2.0)
-        draw(y[0], 'r')
-        draw(y[1], 'b')
-        draw(y[2], 'g')
-        plt.savefig("predict%d.png"%i, dpi=200)
-        plt.close()
         # print the loss
         out = model(train_input)
         loss_print = loss_fn(out, train_target)
-        print("Step: {}, Loss: {}".format(i, loss_print))
+        print(f"Step: {i}: Loss = {loss_print}")
