becb6d44517de641c469d0979e6ac39f57dbdfef
JoannaChang <joanna.changc@gmail.com>
2023-09-02 10:28:52 +0000

    simplify simtools

--- tools/simTools.py
+++ tools/simTools.py
@@ -233,43 +233,43 @@ def get_processed_pyaldata(seed, sim, epoch_fun = None):
 
     return pyal_df
 
-def get_cc_within(dfs, n_components, epoch_fun = None):
-    """
-    Get canonical correlations within trials for each network
+# def get_cc_within(dfs, n_components, epoch_fun = None):
+#     """
+#     Get canonical correlations within trials for each network
 
-    Parameters
-    ----------
-    dfs: list of Pandas dataframes
-        dataframes for simulation data for each network in pyaldata format
-    n_components: int
-        number of components for PCA
-    epoch_fun : function
-        function that takes a trial and returns the epoch to extract
+#     Parameters
+#     ----------
+#     dfs: list of Pandas dataframes
+#         dataframes for simulation data for each network in pyaldata format
+#     n_components: int
+#         number of components for PCA
+#     epoch_fun : function
+#         function that takes a trial and returns the epoch to extract
 
-    Returns
-    -------
-    ccs: list
-        canonical correlations for each network
-    """
-    data = dt.get_data_array(dfs, epoch_fun, area = 'MCx', model = n_components)
+#     Returns
+#     -------
+#     ccs: list
+#         canonical correlations for each network
+#     """
+#     data = dt.get_data_array(dfs, epoch_fun, area = 'MCx', model = n_components)
 
-    n_shared_trial1 = data.shape[2]
-    trialList1 = np.arange(n_shared_trial1)
-    ccs=[]
-    for session, sessionData in enumerate(data):
-        r = []
-        for n in range(params.n_iter*10):
-            params.rng.shuffle(trialList1)
-            # non-overlapping randomised trials
-            trial1 = trialList1[:n_shared_trial1//2]
-            trial2 = trialList1[-(n_shared_trial1//2):]
-            data1 = np.reshape(sessionData[:,trial1,:,:], (-1,n_components))
-            data2 = np.reshape(sessionData[:,trial2,:,:], (-1,n_components))
-            r.append(cca.canoncorr(data1, data2))
-        ccs.append(r)
-    ccs = np.array(ccs)
-    ccs = np.percentile(ccs, 99, axis=1).T
-    return ccs
+#     n_shared_trial1 = data.shape[2]
+#     trialList1 = np.arange(n_shared_trial1)
+#     ccs=[]
+#     for session, sessionData in enumerate(data):
+#         r = []
+#         for n in range(params.n_iter*10):
+#             params.rng.shuffle(trialList1)
+#             # non-overlapping randomised trials
+#             trial1 = trialList1[:n_shared_trial1//2]
+#             trial2 = trialList1[-(n_shared_trial1//2):]
+#             data1 = np.reshape(sessionData[:,trial1,:,:], (-1,n_components))
+#             data2 = np.reshape(sessionData[:,trial2,:,:], (-1,n_components))
+#             r.append(cca.canoncorr(data1, data2))
+#         ccs.append(r)
+#     ccs = np.array(ccs)
+#     ccs = np.percentile(ccs, 99, axis=1).T
+#     return ccs
 
 def get_cc_across(dfs, n_components, epoch_fun = None):
     """
@@ -299,18 +299,8 @@ def get_cc_across(dfs, n_components, epoch_fun = None):
     #setup data
     side1df = [dfs[i] for i,_ in pairFileList1]
     side2df = [dfs[j] for _,j in pairFileList1]
-    AllData1 = dt.get_data_array(side1df, epoch_fun, area='MCx', model=n_components)
-    AllData2 = dt.get_data_array(side2df, epoch_fun, area='MCx', model=n_components)
-    
-    #calculate ccs
-    _,_, min_trials, min_time,_ = np.min((AllData1.shape,AllData2.shape),axis=0)
-    ccs=[]
-    for sessionData1,sessionData2 in zip(AllData1,AllData2):
-        data1 = np.reshape(sessionData1[:,:min_trials,:min_time,:], (-1,n_components))
-        data2 = np.reshape(sessionData2[:,:min_trials,:min_time,:], (-1,n_components))
-        ccs.append(cca.canoncorr(data1, data2))
-    
-    ccs = np.array(ccs).T
+
+    ccs = cca.get_ccs(side1df, side2df, epoch_fun, area='MCx', n_components=n_components)
 
     return ccs
 
@@ -343,18 +333,8 @@ def get_cc_across_groups(dfs1, dfs2, n_components, epoch_fun = None):
     #setup data
     side1df = [dfs1[i] for i,_ in pairFileList1]
     side2df = [dfs2[j] for _,j in pairFileList1]
-    AllData1 = dt.get_data_array(side1df, epoch_fun, area='MCx', model=n_components)
-    AllData2 = dt.get_data_array(side2df, epoch_fun, area='MCx', model=n_components)
 
-    #calculate ccs  
-    _,_, min_trials, min_time,_ = np.min((AllData1.shape,AllData2.shape),axis=0)
-    ccs=[]
-    for sessionData1,sessionData2 in zip(AllData1,AllData2):
-        data1 = np.reshape(sessionData1[:,:min_trials,:min_time,:], (-1,n_components))
-        data2 = np.reshape(sessionData2[:,:min_trials,:min_time,:], (-1,n_components))
-        ccs.append(cca.canoncorr(data1, data2))
-    
-    ccs = np.array(ccs).T
+    ccs = cca.get_ccs(side1df, side2df, epoch_fun, area='MCx', n_components=n_components)
 
     return ccs
 
